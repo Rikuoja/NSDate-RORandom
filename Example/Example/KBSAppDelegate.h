cfa46a62c85cfe25798bf672f57209c1feb22219
Keith Smiley <keithbsmiley@gmail.com>
2014-06-27 10:21:24 -0700

    init

---
+++ Example/Example/KBSAppDelegate.h
@@ -0,0 +1,15 @@
+//
+//  KBSAppDelegate.h
+//  Example
+//
+//  Created by Keith Smiley on 6/27/14.
+//  Copyright (c) 2014 smileykeith. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface KBSAppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+@end
---
+++ Example/Example/main.m
@@ -0,0 +1,18 @@
+//
+//  main.m
+//  Example
+//
+//  Created by Keith Smiley on 6/27/14.
+//  Copyright (c) 2014 smileykeith. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+#import "KBSAppDelegate.h"
+
+int main(int argc, char * argv[])
+{
+    @autoreleasepool {
+        return UIApplicationMain(argc, argv, nil, NSStringFromClass([KBSAppDelegate class]));
+    }
+}
