62522b36ae79f599ec3b75ce7d211c2fbd1a18b9
Rikuoja <riku.oja@kapsi.fi>
2014-12-03 17:56:29 +0200

    Update ExampleTests.m

--- Example/ExampleTests/ExampleTests.m
+++ Example/ExampleTests/ExampleTests.m
@@ -7,7 +7,7 @@
 //
 
 #import <XCTest/XCTest.h>
-#import "NSDate+KBSRandom.h"
+#import "NSDate+RORandom.h"
 
 @interface ExampleTests : XCTestCase
 
@@ -17,19 +17,19 @@
 
 - (void)testExample
 {
-    NSDate *date1 = [NSDate kbs_randomDate];
-    NSDate *date2 = [NSDate kbs_randomDate];
+    NSDate *date1 = [NSDate ro_randomDate];
+    NSDate *date2 = [NSDate ro_randomDate];
 
     XCTAssertFalse([date1 isEqualToDate:date2], @"Random dates should not be equal");
 }
 
 - (void)testFromDateUptoDate {
     for (int i=1; i<10000; i++) {
-        NSDate *date1 = [NSDate kbs_randomDate];
-        NSDate *date2 = [NSDate kbs_randomDate];
+        NSDate *date1 = [NSDate ro_randomDate];
+        NSDate *date2 = [NSDate ro_randomDate];
         NSDate *firstDate=[date1 earlierDate:date2];
         NSDate *lastDate=[date1 laterDate:date2];
-        NSDate *date3 = [NSDate kbs_randomDateFromDate:firstDate uptoDate:lastDate];
+        NSDate *date3 = [NSDate ro_randomDateFromDate:firstDate uptoDate:lastDate];
         NSDateFormatter* dateFormatter = [[NSDateFormatter alloc] init];
         [dateFormatter setDateFormat:@"yyyy-MM-dd"];
         NSLog([NSString stringWithFormat:@"%@/%@/%@",[dateFormatter stringFromDate:firstDate],[dateFormatter stringFromDate:lastDate],[dateFormatter stringFromDate:date3]]);
